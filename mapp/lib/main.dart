6af79674e39a996ec6f00d58c09317e5ecec0e6a
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-10-11 13:07:50 +0530

    Add files via upload

---
+++ mapp/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:musicapp/screens/splashscreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'MusicApp',
+    home: SplashScreen(),
+  ));
+}
